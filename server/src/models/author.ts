89bf286a5ad9a7c130132a893bbed5d82ff14ea8
OmarMohamed95 <omar.mo9516@gmail.com>
2021-06-26 12:47:58 +0200

    Create author model and migration

---
+++ server/src/models/author.ts
@@ -0,0 +1,51 @@
+import { 
+	Sequelize,
+	DataTypes,
+	Model,
+	Association,
+	HasManyGetAssociationsMixin,
+	HasManyAddAssociationMixin,
+	HasManyHasAssociationMixin,
+	HasManyCountAssociationsMixin,
+	HasManyCreateAssociationMixin
+} from 'sequelize';
+import sequelize from '../database/connection'
+import Book from './book'
+
+class Author extends Model {
+	public getBooks!: HasManyGetAssociationsMixin<Book>;
+	public addBook!: HasManyAddAssociationMixin<Book, number>;
+	public hasBook!: HasManyHasAssociationMixin<Book, number>;
+	public countBook!: HasManyCountAssociationsMixin;
+	public createBook!: HasManyCreateAssociationMixin<Book>;
+}
+
+Author.init({
+	id: {
+		type: DataTypes.INTEGER,
+		allowNull: false,
+		autoIncrement: true,
+		primaryKey: true
+	},
+	name: {
+		type: DataTypes.STRING(255),
+		allowNull: false,
+	},
+}, {
+	sequelize,
+	modelName: 'author'
+});
+
+export interface AuthorAttributes {
+  	name ? : string;
+}
+
+export interface AuthorInstance {
+	id: number;
+	createdAt: Date;
+	updatedAt: Date;
+
+	name: string;
+}
+
+export default Author;
